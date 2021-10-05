de22c0da2bc84dcadf70a7e92701fcf30035b85e
Carl Mercier <carl@carlmercier.com>
2021-10-05 19:17:00 -0400

    Expose config files for more flexibility

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,5 +20,23 @@ if [ ! -L /jirafeau/lib/config.local.php ]; then
 	ln -s /cfg/config.local.php /jirafeau/lib/config.local.php
 fi
 
+if [ ! -f /cfg/nginx.conf ]; then
+	cp /etc/nginx/nginx.original.conf /cfg/nginx.conf
+fi
+
+if [ ! -L /etc/nginx/nginx.conf ]; then
+	rm -f /etc/nginx/nginx.conf
+	ln -s /cfg/nginx.conf /etc/nginx/nginx.conf
+fi
+
+if [ ! -f /cfg/php-fpm.conf ]; then
+	cp /etc/php7/php-fpm.original.conf /cfg/php-fpm.conf
+fi
+
+if [ ! -L /etc/php7/php-fpm.conf ]; then
+	rm -f /etc/php7/php-fpm.conf
+	ln -s /cfg/php-fpm.conf /etc/php7/php-fpm.conf
+fi
+
 chown -R jirafeau:jirafeau /jirafeau /var/run/php-fpm.sock /var/lib/nginx /tmp /data /cfg /var/tmp/nginx
 supervisord -c /usr/local/etc/supervisord.conf
